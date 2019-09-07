8cffa7f23e490f030d5e0514fdf8ff73341298cf
Real Ghost <alejandro519@gmail.com>
2019-09-07 03:03:56 -0700

    Added styling elements to the page

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+// Page loads, start button appears
+
+// When start button is clicked, page with the following elements appears:
+
+// 30 second counter
+
+// Question with 5 multiple choice responses, each in a radio button
+
+// When radio button is clicked, choice remains clicked, if another radio button is clicked for the same questions, you can only choose radio button one at a time
+
+// If time runs out, message displayed showing correct answers, incorrect answers and unanswered 
+
+// Done button displayed at the bottom of the page that ends the game on user's command
+
+//Which Backstreet Boy sings the first line in "I Want It That Way"?
+//Brian Littrell
+//AJ Mclean
+//Nick Carter
+//Kevin Richardson
+
